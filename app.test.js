9ca9199a0d0fef9114f7b264b61a50c945a9711d
sanat <sanatmuaythai@gmail.com>
2026-05-24 22:54:50 +0530

    repaired the broken test

--- app.test.js
+++ app.test.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Docker!'));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.get('/info', (req, res) => res.json({ app: 'my app', version: '1.0.0' }));
 
